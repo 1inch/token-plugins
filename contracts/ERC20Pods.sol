5e8bda703d380fd583270317af9734c4fe53454a
Anton Bukov <k06aaa@gmail.com>
2022-10-30 00:16:01 +0200

    Some refactoring

--- contracts/ERC20Pods.sol
+++ contracts/ERC20Pods.sol
@@ -12,59 +12,66 @@ abstract contract ERC20Pods is ERC20, IERC20Pods {
     using AddressSet for AddressSet.Data;
     using AddressArray for AddressArray.Data;
 
-    error AlreadyJoined();
-    error PodIsNotAttached();
+    error PodAlreadyAdded();
+    error PodNotFound();
     error InvalidPodAddress();
     error PodsLimitReachedForAccount();
 
     uint256 private constant _POD_CALL_GAS_LIMIT = 200_000;
 
     uint256 public immutable podsLimit;
+
     mapping(address => AddressSet.Data) private _pods;
 
     constructor(uint256 podsLimit_) {
         podsLimit = podsLimit_;
     }
 
-    function hasPod(address account, address pod) external view returns(bool) {
+    function hasPod(address account, address pod) public view virtual returns(bool) {
         return _pods[account].contains(pod);
     }
 
-    function podsCount(address account) external view returns(uint256) {
+    function podsCount(address account) public view virtual returns(uint256) {
         return _pods[account].length();
     }
 
-    function podAt(address account, uint256 index) external view returns(address) {
+    function podAt(address account, uint256 index) public view virtual returns(address) {
         return _pods[account].at(index);
     }
 
-    function pods(address account) external view returns(address[] memory) {
+    function pods(address account) public view virtual returns(address[] memory) {
         return _pods[account].items.get();
     }
 
-    function addPod(address pod) external override {
+    function addPod(address pod) public virtual {
         if (pod == address(0)) revert InvalidPodAddress();
-        if (!_pods[msg.sender].add(pod)) revert AlreadyJoined();
+        if (!_pods[msg.sender].add(pod)) revert PodAlreadyAdded();
         if (_pods[msg.sender].length() > podsLimit) revert PodsLimitReachedForAccount();
+
         uint256 balance = balanceOf(msg.sender);
         if (balance > 0) {
             _updateBalances(pod, address(0), msg.sender, balance);
         }
     }
 
-    function removePod(address pod) public override {
-        if (!_pods[msg.sender].remove(pod)) revert PodIsNotAttached();
+    function removePod(address pod) public virtual {
+        if (!_pods[msg.sender].remove(pod)) revert PodNotFound();
+
         uint256 balance = balanceOf(msg.sender);
         if (balance > 0) {
             _updateBalances(pod, msg.sender, address(0), balance);
         }
     }
 
-    function removeAllPods() external virtual {
+    function removeAllPods() public virtual {
         address[] memory items = _pods[msg.sender].items.get();
+        uint256 balance = balanceOf(msg.sender);
         unchecked {
             for (uint256 i = items.length; i > 0; i--) {
-                removePod(items[i - 1]);
+                if (balance > 0) {
+                    _updateBalances(items[i - 1], msg.sender, address(0), balance);
+                }
+                _pods[msg.sender].remove(items[i - 1]);
             }
         }
     }
@@ -111,4 +118,3 @@ abstract contract ERC20Pods is ERC20, IERC20Pods {
         }
     }
 }
-
