679f60cff334b84c9b35026f4f290f93b40dd618
Anton Bukov <k06aaa@gmail.com>
2022-11-21 14:30:50 +0100

    Revert "Replace library with base contract"
    This reverts commit ab4ecc27359db82b3e6611ab46c7b2360a239b15.

--- contracts/ERC20Pods.sol
+++ contracts/ERC20Pods.sol
@@ -6,17 +6,57 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@1inch/solidity-utils/contracts/libraries/AddressSet.sol";
 
 import "./interfaces/ITokenPods.sol";
-import "./TokenPods.sol";
+import "./TokenPodsLib.sol";
 
-abstract contract ERC20Pods is ERC20, TokenPods {
-    constructor(uint256 podsLimit) TokenPods(podsLimit) {}
+abstract contract ERC20Pods is ERC20, ITokenPods {
+    using TokenPodsLib for TokenPodsLib.Data;
 
-    function _balanceOf(address account) internal view override virtual returns(uint256) {
-        return balanceOf(account);
+    error PodsLimitReachedForAccount();
+
+    uint256 public immutable podsLimit;
+
+    TokenPodsLib.Data private _pods;
+
+    constructor(uint256 podsLimit_) {
+        podsLimit = podsLimit_;
     }
+
+    function hasPod(address account, address pod) public view virtual returns(bool) {
+        return _pods.hasPod(account, pod);
+    }
+
+    function podsCount(address account) public view virtual returns(uint256) {
+        return _pods.podsCount(account);
+    }
+
+    function podAt(address account, uint256 index) public view virtual returns(address) {
+        return _pods.podAt(account, index);
+    }
+
+    function pods(address account) public view virtual returns(address[] memory) {
+        return _pods.pods(account);
+    }
+
+    function podBalanceOf(address pod, address account) public view virtual returns(uint256) {
+        return _pods.podBalanceOf(account, pod, balanceOf);
+    }
+
+    function addPod(address pod) public virtual {
+        if (_pods.addPod(msg.sender, pod, balanceOf(msg.sender)) > podsLimit) revert PodsLimitReachedForAccount();
+    }
+
+    function removePod(address pod) public virtual {
+        _pods.removePod(msg.sender, pod, balanceOf(msg.sender));
+    }
+
+    function removeAllPods() public virtual {
+        _pods.removeAllPods(msg.sender, balanceOf(msg.sender));
+    }
+
+    // ERC20 Overrides
 
     function _afterTokenTransfer(address from, address to, uint256 amount) internal override virtual {
         super._afterTokenTransfer(from, to, amount);
-        _updatePodsBalances(from, to, amount);
+        _pods.updateBalances(from, to, amount);
     }
 }
