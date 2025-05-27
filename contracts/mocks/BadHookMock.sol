a95060b0c0f5129106aca45504779b52c92e18d5
Anton Bukov <k06aaa@gmail.com>
2025-05-27 16:28:26 +0200

    Fix linter

--- contracts/mocks/BadHookMock.sol
+++ contracts/mocks/BadHookMock.sol
@@ -6,6 +6,8 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IERC20Hooks, Hook } from "../Hook.sol";
 
 contract BadHookMock is ERC20, Hook {
+    error ElPromblemo(string message);
+
     bool public isRevert;
     bool public outOfGas;
     bool public returnGasBomb;
@@ -18,16 +20,31 @@ contract BadHookMock is ERC20, Hook {
     function setIsRevert(bool v) external {
         isRevert = v;
     }
+
     function setOutOfGas(bool v) external {
         outOfGas = v;
     }
+
     function setReturnGasBomb(bool v) external {
         returnGasBomb = v;
     }
 
-    function _updateBalances(address, address, uint256) internal override {
-        if (isRevert) revert("revert");
-        if (outOfGas) while (true) {}
-        if (returnGasBomb) assembly { invalid() }
+    function _updateBalances(address, address, uint256) internal view override {
+        if (isRevert) {
+            revert ElPromblemo("revert");
+        }
+
+        if (outOfGas) {
+            while (true) {
+                outOfGas;
+                // Wasting gas to simulate out of gas
+            }
+        }
+
+        if (returnGasBomb) {
+            assembly ("memory-safe") { // solhint-disable-line no-inline-assembly
+                invalid()
+            }
+        }
     }
 }
